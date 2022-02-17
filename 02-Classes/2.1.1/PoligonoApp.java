41db298a11bceace0c9075618e06e0eab0b6815d
Charles Silva <37charlessilva@gmail.com>
2022-02-17 15:27:17 -0300

    ...

--- 02-Classes/2.1.1/PoligonoApp.java
+++ 02-Classes/2.1.1/PoligonoApp.java
@@ -1,3 +1,23 @@
 public class PoligonoApp {
-    
+    public static void main(String[] args) {
+        Poligono p1 = new Poligono(1, 2, 5, 6, 10);
+        p1.print();
+    }
+}
+
+class Poligono {
+    int x, y;
+    int w, h;
+    int c;
+
+    Poligono (int x, int y, int w, int h, int c) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+    }
+    void print() {
+        System.out.format("Poligono de tamanho (%d, %d) na posicao (%d,%d) com %d lado(s).\n", this.w, this.h, this.x, this.y, this.c);
+    } 
 }
